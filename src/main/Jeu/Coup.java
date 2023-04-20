ce8b0f95758af7081d73e1a1642308870102f1ea
Combe Gaetan <combegae@im2ag-202-15.e-im2ag.ujf-grenoble.fr>
2023-04-20 16:24:15 +0200

    Mise en place Jeu avec méthodes joue, annule etc, et Coup

---
+++ src/main/Jeu/Coup.java
@@ -0,0 +1,18 @@
+
+
+
+public class Coup {
+    int l,c;
+
+
+
+Coup(int ligne, int colonne){
+    this.l = ligne;
+    this.c = colonne;
+}
+public Coup cloner(){
+    Coup cp = new Coup(this.l,this.c);
+    return cp;
+}
+
+}
